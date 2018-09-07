24e60133ddc1746ab8a7caf38e88e7f4ee3db8d6
haomingzhang0101 <haomingzhang0101@gmail.com>
2018-09-06 20:39:39 -0700

    Add several annotations for marking different type of classes.

---
+++ src/main/java/com/haoming/concurrency/annotations/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.haoming.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * For marking recommend classes only.
+ * @author zhanghm
+ * @date 2018-09-06 20:34
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
---
+++ src/main/java/com/haoming/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,18 @@
+package com.haoming.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * For marking thread-safe classes only.
+ * @author zhanghm
+ * @date 2018-09-06 20:34
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
