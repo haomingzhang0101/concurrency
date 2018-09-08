292299589a1afdceb99f4afda60bfbbbc5cb684f
haomingzhang0101 <haomingzhang0101@gmail.com>
2018-09-07 19:17:59 -0700

    Add examples for synchronized keyword.

---
+++ src/main/java/com/haoming/concurrency/example/sync/SynchronizedExample1.java
@@ -0,0 +1,40 @@
+package com.haoming.concurrency.example.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class SynchronizedExample1 {
+
+    public void test1(int j) {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                log.info("test1 {} - {}", j, i);
+            }
+        }
+    }
+
+    // Synchronized method. Note that this keyword can not be extended by a subclass.
+    public synchronized void test2(int j) {
+        for (int i = 0; i < 10; i++) {
+            log.info("test2 {} - {}", j, i);
+        }
+    }
+
+    public static void main(String[] args)  {
+        SynchronizedExample1 example1 = new SynchronizedExample1();
+        SynchronizedExample1 example2 = new SynchronizedExample1();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(() -> {
+            example1.test1(1);
+        });
+        executorService.execute(() -> {
+            example2.test1(2);
+        });
+
+        executorService.shutdown();
+
+    }
+}
